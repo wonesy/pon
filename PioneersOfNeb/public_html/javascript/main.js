9bbb4dbf0087099c256fc606d6cd31df87b1b246
Cameron <wonesy@wonesy-ubuntu.(none)>
2013-06-09 17:42:37 -0400

    added more the the player banners, showing values and images in the correct locations

--- PioneersOfNeb/public_html/javascript/main.js
+++ PioneersOfNeb/public_html/javascript/main.js
@@ -6,11 +6,13 @@
 //var globalGameBoard = null;
 var globalGame = null;
 var conn = null;
-var gameHasStarted;
+var gameHasStarted = false;
 var gameCanvasElement = null;
 var gameCanvasContext = null;
 var hudCanvasElement = null;
 var hudCanvasContext = null;
+var menuCanvasElement = null;
+var menuCanvasContext = null;
 
 window.onload = function() {
     console.log("on load");
@@ -24,13 +26,21 @@ window.onload = function() {
     hudCanvasElement.setAttribute("width", hudCanvasElement.parentNode.offsetWidth);
     hudCanvasElement.setAttribute("height", hudCanvasElement.parentNode.offsetHeight);
     
+    menuCanvasElement = document.getElementById("menuCanvas");
+    menuCanvasContext = menuCanvasElement.getContext('2d');
+    menuCanvasElement.setAttribute("width", menuCanvasElement.parentNode.offsetWidth);
+    menuCanvasElement.setAttribute("height", menuCanvasElement.parentNode.offsetHeight);
+    
     // EVENT LISTENERS
     hudCanvasElement.addEventListener("click", hudOnClick, false);
     
     globalGame = new Game(4, gameCanvasElement.width, gameCanvasElement.height);
     globalGame.sizeBoard(gameCanvasElement.width, gameCanvasElement.height);
     //globalGame.drawAll(gameCanvasContext);
-    run_randomizeMenu();
+    if (!gameHasStarted) {
+        resetButtons();
+        showRandomizeMenu();
+    }
 };
 
 window.onresize = function() {
@@ -45,10 +55,15 @@ window.onresize = function() {
     hudCanvasElement.setAttribute("width", hudCanvasElement.parentNode.offsetWidth);
     hudCanvasElement.setAttribute("height", hudCanvasElement.parentNode.offsetHeight);
     
+    menuCanvasElement = document.getElementById("menuCanvas");
+    menuCanvasContext = menuCanvasElement.getContext('2d');
+    menuCanvasElement.setAttribute("width", menuCanvasElement.parentNode.offsetWidth);
+    menuCanvasElement.setAttribute("height", menuCanvasElement.parentNode.offsetHeight);
+    
     globalGame.sizeBoard(gameCanvasElement.width, gameCanvasElement.height);
-    globalGame.drawAll(gameCanvasContext);
+    globalGame.drawAll(gameCanvasContext, hudCanvasContext);
     if (!gameHasStarted) {
         resetButtons();
-        run_randomizeMenu();
+        showRandomizeMenu();
     }
 };
